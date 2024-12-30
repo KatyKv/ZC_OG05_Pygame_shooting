ecec6e6182e9dcbd348bb6bf72f8074ab4f3fea5
Ekaterina <kate.kvacheva@gmail.com>
2024-12-30 18:03:52 +0500

    main.py - playing mechanics added
    Added function: new_target_place.
    To the game loop, added processing of the MOUSEBUTTONDOWN event: the target changes its place.

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import pygame
 import random
 
+def new_target_place(object_width, object_height):
+    x = random.randint(0, SCREEN_WIDTH - object_width)
+    y = random.randint(0, SCREEN_HEIGHT - object_height)
+    return x, y
+
 pygame.init()
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -12,8 +17,7 @@ pygame.display.set_icon(icon)
 target_img = pygame.image.load('./img/target.png')
 target_width = 80
 target_height = 80
-target_x = random.randint(0, SCREEN_WIDTH - target_width)
-target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+target_x, target_y = new_target_place(target_width, target_height)
 color = (random.randint(0, 255),
          random.randint(0, 255),
          random.randint(0, 255))
@@ -24,7 +28,15 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if (target_x <= mouse_x <= target_x + target_width and
+                target_y <= mouse_y <= target_y + target_height):
+                target_x, target_y = new_target_place(target_width, target_height)
+
+
     screen.blit(target_img, (target_x, target_y))
+
 
 
 
