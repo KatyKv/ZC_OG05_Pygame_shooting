75fc320abff7736a7012301e39c4b889caa9a65d
Ekaterina <kate.kvacheva@gmail.com>
2024-12-30 17:39:46 +0500

    Update main: screen color, taget_img, fixed name heighT
    To game cycle added:
    - screen_fill,
    - event QUIT processing,
    - placing a target_img to the screen,
    - display updating.
    
    Fixed name SCREEN_HEIGHT.
    Removed unnecessary import.

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-from random import randint, random
-
 import pygame
 import random
 
 pygame.init()
 SCREEN_WIDTH = 800
-SCREEN_HEIGH = 600
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGH))
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Игра - Сбей мишень')
 
 icon = pygame.image.load('./img/icon_shoot.png')
@@ -15,17 +13,20 @@ target_img = pygame.image.load('./img/target.png')
 target_width = 80
 target_height = 80
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
-target_y = random.randint(0, SCREEN_HEIGH - target_height)
+target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 color = (random.randint(0, 255),
          random.randint(0, 255),
          random.randint(0, 255))
 
-
 running = True
 while running:
-    pass
+    screen.fill(color)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.blit(target_img, (target_x, target_y))
 
 
 
-
+    pygame.display.update()
 pygame.quit()
