14026bef74438bfece6c50c10a663cb9a5eb231d
Ekaterina <kate.kvacheva@gmail.com>
2024-12-30 19:39:49 +0500

    Add counter of hits and misses

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Игра - Сбей мишень')
 
+pygame.font.init()
+font = pygame.font.SysFont('Arial', 36)
+
 icon = pygame.image.load('./img/icon_shoot.png')
 pygame.display.set_icon(icon)
 target_img = pygame.image.load('./img/target.png')
@@ -20,10 +23,14 @@ target_height = 80
 target_x, target_y = new_target_place(target_width, target_height)
 boom_img = pygame.image.load('./img/boom.png')
 
-
 color = (random.randint(0, 255),
          random.randint(0, 255),
          random.randint(0, 255))
+times_to_win = 5
+count_hit = 0
+count_miss = 0
+text_color = 'white'
+
 
 last_move_time = pygame.time.get_ticks()
 move_interval = 1000 # В миллисекундах = 1 сек
@@ -43,8 +50,10 @@ while running:
                 target_y <= mouse_y <= target_y + target_height):
                 show_boom = True
                 boom_start_time = pygame.time.get_ticks()
-#                target_x, target_y = new_target_place(target_width, target_height)
-#                last_move_time = pygame.time.get_ticks()
+                count_hit += 1
+            else:
+                count_miss += 1
+
 
     current_time = pygame.time.get_ticks()
     if show_boom:
@@ -61,8 +70,17 @@ while running:
     if not show_boom:
         screen.blit(target_img, (target_x, target_y))
 
+    if count_hit == 0 and count_miss == 0:
+        text = f'Привет! Сбей пчелу {times_to_win} раз!'
+    else:
+        text = f'Попал: {count_hit}, промазал: {count_miss}.'
+    text_surface = font.render(text, True, text_color)
+    screen.blit(text_surface, (5, 5))
 
-
+    if count_hit == times_to_win:
+        pygame.display.flip()
+        pygame.time.delay(5000)
+        running = False
 
     pygame.display.flip()
 pygame.quit()
